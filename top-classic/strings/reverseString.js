401601fe8ed8be698e703d6b8f176a9744500484
denvermullets <devmullets@gmail.com>
2020-10-27 08:23:39 -0400

    :recycle: add comments for review

--- top-classic/strings/reverseString.js
+++ top-classic/strings/reverseString.js
@@ -1,3 +1,11 @@
+// Write a function that reverses a string. The input string is given as an array of
+// characters char[].
+//
+// Do not allocate extra space for another array, you must do this by modifying the
+// input array in-place with O(1) extra memory.
+//
+// You may assume all the characters consist of printable ascii characters.
+
 var reverseString = function(s) {
   // opposite directional two-pointer approach
   // take each letter from each end and work inwards until the whole array is reversed
@@ -13,7 +21,7 @@ var reverseString = function(s) {
     right--
   }
   
-  console.log(s)
+  console.log('script executed', s)
 }
 
 const tempArray = ['h', 'e', 'l', 'l', 'o']
